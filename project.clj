b2fbff9a84fdc0828af0b3bee32616a0d7fcc023
Tom Jakubowski <tom@crystae.net>
2014-01-26 03:22:10 -0800

    add license etc

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject weasel "0.1.0-SNAPSHOT"
   :description "websocket repl for clojurescript"
   :url "http://github.com/tomjakubowski/weasel"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/UNLICENSE"
+            :distribution :repo}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
-                 [com.cemerick/piggieback "0.1.2"]
                  [org.clojure/google-closure-library "0.0-20130212-95c19e7f0f5f"]
                  [http-kit "2.1.16"]]
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.2"]]}}
 
   :plugins [[lein-cljsbuild "1.0.1"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
