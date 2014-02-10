fccc84be4d65c346f89c9bddc1b84a8d371a6806
Tom Jakubowski <tom@crystae.net>
2014-02-10 03:12:13 -0800

    cleanup

--- src/cljs/weasel/repl.cljs
+++ src/cljs/weasel/repl.cljs
@@ -6,11 +6,6 @@
 
 (def ws-connection (atom nil))
 
-;;; TODO: needs to connect to something that implements cljs.repl/IJavaScriptEnv
-;;; see repl/browser.clj
-;;; repl/server.clj
-;;; repl.clj
-
 (defmulti process-message :op)
 
 (defmethod process-message
@@ -30,16 +25,18 @@
   [repl-server-url]
   (let [repl-connection (ws/websocket-connection)]
     (swap! ws-connection (constantly repl-connection))
+
     (event/listen repl-connection :opened
       (fn [evt]
-        (.log js/console "Opened WS connection!")))
+        (.info js/console "Opened WS connection!")))
+
     (event/listen repl-connection :message
       (fn [evt]
         (let [{:keys [op] :as message} (read-string (.-message evt))
               response (-> message process-message pr-str)]
-          #_(println "got a message" op "from" message "... dispatching ...")
-          #_(println "sending to server:" response)
           (net/transmit repl-connection response))))
+
     (event/listen repl-connection :error
       (fn [evt] (.error js/console "WebSocket error" evt)))
+
     (net/connect repl-connection repl-server-url)))
