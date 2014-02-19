0125fcd814a90fd2ef9c4d5ac59907375c323406
Tom Jakubowski <tom@crystae.net>
2014-02-18 18:13:18 -0800

    remove old example code

--- src/cljs/weasel/example.cljs
+++
@@ -1,8 +0,0 @@
-(ns weasel.example
-  (:require [weasel.repl :as ws-repl]
-            #_[clojure.browser.repl :as cljs-repl]))
-
-(enable-console-print!)
-
-#_(cljs-repl/connect "http://localhost:9000/repl")
-(ws-repl/connect "ws://localhost:9001/" :verbose true)
