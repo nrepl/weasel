3d0bb719829ab0567481c18f18f8b27f3999592a
Tom Jakubowski <tom@crystae.net>
2014-10-20 03:45:03 -0700

    Version 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weasel "0.4.1-SNAPSHOT"
+(defproject weasel "0.4.1"
   :description "websocket REPL environment for ClojureScript"
   :url "http://github.com/tomjakubowski/weasel"
   :license {:name "Unlicense"
