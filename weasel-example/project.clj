ac9082fe60ca1a8c0036c3e3738b9d2fe4bd460e
Tom Jakubowski <tom@crystae.net>
2015-01-11 21:09:40 -0800

    More example updates, add note about Piggieback

--- weasel-example/project.clj
+++ weasel-example/project.clj
@@ -7,10 +7,10 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2665"]
-                 [weasel "0.4.3-SNAPSHOT"]]
+                 [weasel "0.5.0-SNAPSHOT"]]
 
   :source-paths ["src"]
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.4"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.5-SNAPSHOT"]]
                    :plugins [[lein-cljsbuild "1.0.3"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :cljsbuild {:builds [{:id "weasel-example"
